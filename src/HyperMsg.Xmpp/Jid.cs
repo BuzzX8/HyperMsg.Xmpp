23ce7f3f0d1ccbd2a658f6c9025249e79258e6fc
Maxim Chasnik <maxim.chasnik@outlook.com>
2018-09-22 17:00:23 +0300

    Migrated Jid, XmlElement and XmlElementExtensions

---
+++ src/HyperMsg.Xmpp/Jid.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HyperMsg.Xmpp
+{
+	public class Jid
+	{
+		public const int MaxJidPartByteLength = 1023;
+
+		private static readonly char[] InvalidUserPartChars = { '@', '/' };
+		private static readonly char[] InvalidDomainPartChars = { '@', '/' };
+		private static readonly char[] InvalidResourcePartChars = { '/' };
+		private string user;
+		private string domain;
+		private string resource;
+
+		public Jid(string bareJid)
+		{
+			if (!IsValid(bareJid)) throw new ArgumentException("Invalid format for Jid");
+
+			int domSeparatorPos = bareJid.IndexOf('@');
+			int resSeparatorPos = bareJid.IndexOf('/');
+			int domainStrLen = resSeparatorPos < 0 ? bareJid.Length - domSeparatorPos - 1 : resSeparatorPos - domSeparatorPos - 1;
+
+			User = domSeparatorPos < 0 ? "" : bareJid.Substring(0, domSeparatorPos);
+			Domain = bareJid.Substring(domSeparatorPos + 1, domainStrLen);
+			Resource = resSeparatorPos < 0 ? "" : bareJid.Substring(resSeparatorPos + 1, bareJid.Length - resSeparatorPos - 1);
+		}
+
+		public Jid(string user, string domain) : this(user, domain, null)
+		{ }
+
+		public Jid(string user, string domain, string resource)
+		{
+			User = user;
+			Domain = domain;
+			Resource = resource;
+		}
+
+		public string User
+		{
+			get { return user; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value)) ValidateUserFormat(value);
+				user = value;
+			}
+		}
+		public string Domain
+		{
+			get { return domain; }
+			set
+			{
+				ValidateDomainFormat(value);
+				domain = value;
+			}
+		}
+		public string Resource
+		{
+			get { return resource; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value)) ValidateResourceFormat(value);
+				resource = value;
+			}
+		}
+
+		private void ValidateUserFormat(string user)
+		{
+			ValidateUserPartCharacters(user);
+			if (!IsValidPartLength(user)) throw new ArgumentException();
+		}
+
+		private void ValidateUserPartCharacters(string user)
+		{
+			ValidateCharacters(user, InvalidUserPartChars, "");
+		}
+
+		private void ValidateDomainFormat(string domain)
+		{
+			ValidateDomainPartCharacters(domain);
+			if (!IsValidPartLength(domain)) throw new ArgumentException();
+		}
+
+		private void ValidateDomainPartCharacters(string domain)
+		{
+			ValidateCharacters(domain, InvalidDomainPartChars, "");
+		}
+
+		public void ValidateResourceFormat(string resource)
+		{
+			ValidateResourcePartCharacters(resource);
+			if (!IsValidPartLength(resource)) throw new ArgumentException();
+		}
+
+		private void ValidateResourcePartCharacters(string resource)
+		{
+			ValidateCharacters(resource, InvalidResourcePartChars, "");
+		}
+
+		private void ValidateCharacters(string jidPart, char[] invalidCharacters, string exceptionMessage)
+		{
+			int invalidCharIndex = jidPart.IndexOfAny(invalidCharacters);
+			if (invalidCharIndex > -1) throw new ArgumentException(string.Format(exceptionMessage, jidPart[invalidCharIndex]));
+		}
+
+		public override int GetHashCode()
+		{
+			return ToString().GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			if (!string.IsNullOrEmpty(User))
+			{
+				sb.Append(User);
+				sb.Append('@');
+			}
+			sb.Append(Domain);
+			if (!string.IsNullOrEmpty(Resource))
+			{
+				sb.Append('/');
+				sb.Append(Resource);
+			}
+			return sb.ToString();
+		}
+
+		public static Jid Parse(string bareJid)
+		{
+			return new Jid(bareJid);
+		}
+
+		public static implicit operator Jid(string bareJid)
+		{
+			return new Jid(bareJid);
+		}
+
+		public static implicit operator string(Jid jid)
+		{
+			return jid.ToString();
+		}
+
+		private static bool IsValidPartLength(string jidPart)
+		{
+			return Encoding.UTF8.GetByteCount(jidPart) <= MaxJidPartByteLength;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null) return false;
+			if (!(obj is Jid)) return false;
+			Jid jid = (Jid)obj;
+			return Equals(jid);
+		}
+
+		public bool Equals(Jid jid)
+		{
+			return User == jid.User &&
+				   Domain == jid.Domain &&
+				   Resource == jid.Resource;
+		}
+
+		public static bool IsValid(string bareJid)
+		{
+			return Regex.IsMatch(bareJid, @"(\S+@)?\w+(\.\w{3})?(/\w*)?");
+		}
+	}
+}
---
+++ src/HyperMsg.Xmpp/XmlElementExtensions.cs
@@ -0,0 +1,195 @@
+﻿using System.Linq;
+
+namespace HyperMsg.Xmpp
+{
+    /// <summary>
+    /// Provides extension methods for fluent manipulation of XmlElement
+    /// </summary>
+    public static class XmlElementExtensions
+    {
+        /// <summary>
+        /// Updates attribute value for XmlElement.
+        /// </summary>
+        /// <param name="element">
+        /// Element which attribute value should be updated.
+        /// </param>
+        /// <param name="name">
+        /// Attribute name.
+        /// </param>
+        /// <param name="value">
+        /// Attribute value.
+        /// </param>
+        /// <returns>
+        /// Updated XmlElement.
+        /// </returns>
+        public static XmlElement Attribute(this XmlElement element, string name, object value)
+        {
+            element.SetAttributeValue(name, value);
+
+            return element;
+        }
+
+        /// <summary>
+        /// Updates attribute value for XmlElement.
+        /// </summary>
+        /// <param name="element">
+        /// Element which attribute value should be updated.
+        /// </param>
+        /// <param name="name">
+        /// Attribute name.
+        /// </param>
+        /// <param name="value">
+        /// Attribute value.
+        /// </param>
+        /// <returns>
+        /// Updated XmlElement.
+        /// </returns>
+        public static XmlElement Attribute(this XmlElement element, string name, string value)
+        {
+            element.SetAttributeValue(name, value);
+
+            return element;
+        }
+
+        /// <summary>
+        /// Returns child element with specified name or null if no child with such name.
+        /// </summary>
+        /// <param name="element">
+        /// Element which child elements should be scanned.
+        /// </param>
+        /// <param name="childName">
+        /// Name of child element.
+        /// </param>
+        /// <returns>
+        /// Child XML element with specified name of null if no elements with such name.
+        /// </returns>
+        public static XmlElement Child(this XmlElement element, string childName)
+        {
+            return element.Children.FirstOrDefault(e => e.Name == childName);
+        }
+
+        /// <summary>
+        /// Adds a set of xml elements as child elements for XmlElement.
+        /// </summary>
+        /// <param name="element">
+        /// Element to which add child elements.
+        /// </param>
+        /// <param name="children">
+        /// Elements which should be added as child elements.
+        /// </param>
+        /// <returns>
+        /// Updated XmlElement.
+        /// </returns>
+        public static XmlElement Children(this XmlElement element, params XmlElement[] children)
+        {
+            foreach (var child in children)
+            {
+                element.Children.Add(child);
+            }
+
+            return element;
+        }
+
+        /// <summary>
+        /// Returns true if element contains child with specified name, otherwise false.
+        /// </summary>
+        /// <param name="element">
+        /// Element which will be checked for existence of child element.
+        /// </param>
+        /// <param name="childName">
+        /// Name of child element.
+        /// </param>
+        /// <returns>
+        /// true if current element has child with specified name, otherwise false.
+        /// </returns>
+        public static bool HasChild(this XmlElement element, string childName)
+        {
+            return element.Children.Any(e => e.Name == childName);
+        }
+
+        /// <summary>
+        /// Assignes <paramref name="value"/> to Value property of XmlElement.
+        /// </summary>
+        /// <param name="element">
+        /// Element which value should be updated.
+        /// </param>
+        /// <param name="value">
+        /// New value of XmlElement.
+        /// </param>
+        /// <returns>
+        /// Element with updated value.
+        /// </returns>
+        public static XmlElement Value(this XmlElement element, string value)
+        {
+            element.Value = value;
+
+            return element;
+        }
+
+        /// <summary>
+        /// Returns value of xmlns attribute or null if element does not contains such attribute.
+        /// </summary>
+        /// <param name="element">
+        /// Element which attribute value should be readed
+        /// </param>
+        /// <returns>
+        /// Value of xmlns attribute or null if no such attribute exists.
+        /// </returns>
+        public static string Xmlns(this XmlElement element)
+        {
+            return element.GetAttributeValue("xmlns");
+        }
+
+        /// <summary>
+        /// Updates value of xmlns attribute.
+        /// </summary>
+        /// <param name="element">
+        /// Element which xmlns attribute should be updated.
+        /// </param>
+        /// <param name="xmlns">
+        /// Value of xmlns attribute.
+        /// </param>
+        /// <returns>
+        /// Updated XmlElement.
+        /// </returns>
+        public static XmlElement Xmlns(this XmlElement element, string xmlns)
+        {
+            element.SetAttributeValue("xmlns", xmlns);
+
+            return element;
+        }
+
+        /// <summary>
+        /// Returns value of xml:lang attribute or null if element does not contains such attribute.
+        /// </summary>
+        /// <param name="element">
+        /// Element which attribute value should be readed
+        /// </param>
+        /// <returns>
+        /// Value of xml:lang attribute or null if no such attribute exists.
+        /// </returns>
+        public static string XmlLang(this XmlElement element)
+        {
+            return element.GetAttributeValue("xml:lang");
+        }
+
+        /// <summary>
+        /// Updates value of xml:lang attribute.
+        /// </summary>
+        /// <param name="element">
+        /// Element which xmlns attribute should be updated.
+        /// </param>
+        /// <param name="xmlns">
+        /// Value of xml:lang attribute.
+        /// </param>
+        /// <returns>
+        /// Updated XmlElement.
+        /// </returns>
+        public static XmlElement XmlLang(this XmlElement element, string lang)
+        {
+            element.SetAttributeValue("xml:lang", lang);
+
+            return element;
+        }
+    }
+}
