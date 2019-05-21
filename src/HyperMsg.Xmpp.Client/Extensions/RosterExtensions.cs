ee25b74c81052236cc38e3b766e88328e2580cf2
Maxim Chasnik <maksym.c@sbtech.com>
2019-05-21 14:33:37 +0300

    wip

---
+++ src/HyperMsg.Xmpp.Client/Extensions/RosterExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+
+namespace HyperMsg.Xmpp.Client.Extensions
+{
+    public static class RosterExtensions
+    {
+        public static string SendRosterRequest(this ISender<XmlElement> channel)
+        {
+            var stanza = CreateRosterRequest();
+            return channel.SendWithNewId(stanza);
+        }
+
+        public static Task<string> SendRosterRequestAsync(this ISender<XmlElement> channel)
+        {
+            var stanza = CreateRosterRequest();
+            return channel.SendWithNewIdAsync(stanza);
+        }
+
+        public static string SendAddOrUpdateItemRequest(this ISender<XmlElement> channel, Jid jid, string name = null, params string[] groups)
+        {
+            var stanza = CreateAddOrUpdateItemRequest(jid, name, groups);
+            return channel.SendWithNewId(stanza);
+        }
+
+        public static Task<string> SendAddOrUpdateItemRequestAsync(this ISender<XmlElement> channel, Jid jid, string name = null, params string[] groups)
+        {
+            var stanza = CreateAddOrUpdateItemRequest(jid, name, groups);
+            return channel.SendWithNewIdAsync(stanza);
+        }
+
+        public static string SendRemoveItemRequest(this ISender<XmlElement> channel, Jid itemJid)
+        {
+            var stanza = CreateRemoveItemRequest(itemJid);
+            return channel.SendWithNewId(stanza);
+        }
+
+        public static Task<string> SendRemoveItemRequestAsync(this ISender<XmlElement> channel, Jid itemJid)
+        {
+            var stanza = CreateRemoveItemRequest(itemJid);
+            return channel.SendWithNewIdAsync(stanza);
+        }
+
+        private static XmlElement CreateRosterRequest()
+        {
+            return AttachQuery(Iq.Get());
+        }
+
+        private static XmlElement CreateAddOrUpdateItemRequest(Jid itemJid, string name, string[] groups)
+        {
+            var item = new XmlElement("item").Attribute("jid", itemJid);
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                item.SetAttributeValue("name", name);
+            }
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                item.Children.Add(new XmlElement("group").Value(groups[i]));
+            }
+
+            return AttachQuery(Iq.Set(), item);
+        }
+
+        private static XmlElement CreateRemoveItemRequest(Jid itemJid)
+        {
+            var itemElement = new XmlElement("item");
+            itemElement.SetAttributeValue("jid", itemJid);
+            itemElement.SetAttributeValue("subscription", "remove");
+
+            return AttachQuery(Iq.Set(), itemElement);
+        }
+
+        private static XmlElement AttachQuery(XmlElement element, params XmlElement[] items)
+        {
+            var query = new XmlElement("query").Xmlns(XmppNamespaces.Roster);
+            element.Children.Add(query);
+            query.Children(items);
+
+            return element;
+        }
+    }
+}
---
+++ test/HyperMsg.Xmpp.Client.Tests/Extensions/RosterExtensionTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg.Xmpp.Client.Extensions
+{
+    public class RosterExtensionTests : SenderExtensionTestBase
+    {
+        private readonly Jid itemJid = "some-user@domain";
+        private readonly string itemName = "SomeUser";
+        private readonly string[] itemGroups = { "Group1", "Group2" };
+
+        [Fact]
+        public void SendRosterRequest_Sends_Correct_Iq_Stanza()
+        {
+            var expectedStanza = Iq.Get().Children(Query());
+
+            VerifySendMethod((c, e) => c.SendRosterRequest(), expectedStanza);
+        }
+
+        [Fact]
+        public async Task SendRosterRequestAsync_Sends_Correct_Iq_Stanza()
+        {
+            var expectedStanza = Iq.Get().Children(Query());
+
+            await VerifySendAsyncMethod((c, e) => c.SendRosterRequestAsync(), expectedStanza);
+        }
+
+        [Fact]
+        public void SendAddOrUpdateItemRequest_Sends_Item_With_Correct_Jid()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid)));
+
+            VerifySendMethod((c, e) => c.SendAddOrUpdateItemRequest(itemJid), expectedStanza);
+        }
+
+        [Fact]
+        public async Task SendAddOrUpdateItemRequestAsync_Sends_Item_With_Correct_Jid()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid)));
+
+            await VerifySendAsyncMethod((s, e) => s.SendAddOrUpdateItemRequestAsync(itemJid), expectedStanza);
+        }
+
+        [Fact]
+        public void SendAddOrUpdateItemRequest_Sends_Item_With_Correct_Jid_And_Name()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid, itemName)));
+
+            VerifySendMethod((s, e) => s.SendAddOrUpdateItemRequest(itemJid, itemName), expectedStanza);
+        }
+
+        [Fact]
+        public async Task SendAddOrUpdateItemRequestAsync_Sends_Item_With_Correct_Jid_And_Name()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid, itemName)));
+
+            await VerifySendAsyncMethod((s, e) => s.SendAddOrUpdateItemRequestAsync(itemJid, itemName), expectedStanza);
+        }
+
+        [Fact]
+        public void SendAddOrUpdateItemRequest_Sends_Item_With_Correct_Jid_Name_And_Groups()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid, itemName, itemGroups)));
+
+            VerifySendMethod((s, e) => s.SendAddOrUpdateItemRequest(itemJid, itemName, itemGroups), expectedStanza);
+        }
+
+        [Fact]
+        public async Task SendAddOrUpdateItemRequestAsync_Sends_Item_With_Correct_Jid_Name_And_Groups()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid, itemName, itemGroups)));
+
+            await VerifySendAsyncMethod((s, e) => s.SendAddOrUpdateItemRequestAsync(itemJid, itemName, itemGroups), expectedStanza);
+        }
+
+        [Fact]
+        public void SendRemoveItemRequest_Sends_Correct_Iq_Stanza()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid).Attribute("subscription", "remove")));
+
+            VerifySendMethod((s, e) => s.SendRemoveItemRequest(itemJid), expectedStanza);
+        }
+
+        [Fact]
+        public async Task SendRemoveItemRequestAsync_Sends_Correct_Iq_Stanza()
+        {
+            var expectedStanza = Iq.Set().Children(
+                Query(Item(itemJid).Attribute("subscription", "remove")));
+
+            await VerifySendAsyncMethod((s, e) => s.SendRemoveItemRequestAsync(itemJid), expectedStanza);
+        }
+
+        private XmlElement Query(params XmlElement[] children) => new XmlElement("query", children).Xmlns(XmppNamespaces.Roster);
+
+        private XmlElement Item(string jid, string name = null, params string[] groups)
+        {
+            return new XmlElement("item")
+                .Attribute("jid", jid)
+                .Attribute("name", name)
+                .Children(groups
+                    .Select(g => new XmlElement("group").Value(g))
+                    .ToArray());
+        }
+    }
+}
