bbee1a5c9020ecb1ad7d3dfa29ad17d44e597cc0
Maxim Chasnik <maksym.c@sbtech.com>
2019-05-21 14:56:16 +0300

    wip

---
+++ src/HyperMsg.Xmpp/Message.cs
@@ -0,0 +1,54 @@
+﻿namespace HyperMsg.Xmpp
+{
+    public static class Message
+    {
+        public static class Type
+        {
+            public static readonly string Chat = "chat";
+            public static readonly string Error = "error";
+            public static readonly string GroupChat = "groupchat";
+            public static readonly string Headline = "headline";
+            public static readonly string Normal = "normal";
+        }
+
+        public static XmlElement New()
+        {
+            return new XmlElement("message");
+        }
+
+        public static XmlElement Chat()
+        {
+            return New().Type(Type.Chat);
+        }
+
+        public static XmlElement Error()
+        {
+            return New().Type(Type.Error);
+        }
+
+        public static XmlElement GroupChat()
+        {
+            return New().Type(Type.GroupChat);
+        }
+
+        public static XmlElement Headline()
+        {
+            return New().Type(Type.Headline);
+        }
+
+        public static XmlElement Normal()
+        {
+            return New().Type(Type.Normal);
+        }
+
+        public static XmlElement Subject(this XmlElement stanza, string subject)
+        {
+            return stanza.Children(new XmlElement("subject").Value(subject));
+        }
+
+        public static XmlElement Body(this XmlElement stanza, string body)
+        {
+            return stanza.Children(new XmlElement("body").Value(body));
+        }
+    }
+}
---
+++ test/HyperMsg.Xmpp.Tests/MessageTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HyperMsg.Xmpp.Tests
+{
+    public class MessageTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new XmlElement("message"), Message.New() };
+            yield return new object[] { new XmlElement("message").Type("chat"), Message.Chat() };
+            yield return new object[] { new XmlElement("message").Type("error"), Message.Error() };
+            yield return new object[] { new XmlElement("message").Type("groupchat"), Message.GroupChat() };
+            yield return new object[] { new XmlElement("message").Type("headline"), Message.Headline() };
+            yield return new object[] { new XmlElement("message").Type("normal"), Message.Normal() };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void MessageCreateMethodTest(XmlElement expectedStanza, XmlElement actualStanza)
+        {
+            Assert.Equal(expectedStanza, actualStanza);
+        }
+
+        [Fact]
+        public void Subject_Adds_Subject_Child()
+        {
+            var expectedSubject = Guid.NewGuid().ToString();
+            var stanza = Message.New().Subject(expectedSubject);
+
+            var actualSubject = stanza.Child("subject").Value;
+
+            Assert.Equal(expectedSubject, actualSubject);
+        }
+
+        [Fact]
+        public void Body_Adds_Body_Child()
+        {
+            var expectedBody = Guid.NewGuid().ToString();
+            var stanza = Message.New().Body(expectedBody);
+
+            var actualBody = stanza.Child("body").Value;
+
+            Assert.Equal(expectedBody, actualBody);
+        }
+    }
+}
