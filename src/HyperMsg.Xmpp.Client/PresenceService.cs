7c3482647e5a620d5afa95b6853bfc06de98036f
Maxim Chasnik <maksym.c@sbtech.com>
2019-07-15 17:18:07 +0300

    wip

---
+++ src/HyperMsg.Xmpp.Client/PresenceService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public class PresenceService : IPresenceService
+    {
+        private readonly IMessageSender<XmlElement> messageSender;
+        private readonly Jid jid;
+
+        public PresenceService(IMessageSender<XmlElement> messageSender, Jid jid)
+        {
+            this.messageSender = messageSender ?? throw new ArgumentNullException(nameof(messageSender));
+            this.jid = jid ?? throw new ArgumentNullException(nameof(jid));
+        }
+
+        public Task UpdateStatusAsync(PresenceStatus presenceStatus, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<PresenceStatus> StatusUpdateReceived;
+    }
+}
---
+++ test/HyperMsg.Xmpp.Client.Tests/PresenceServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using FakeItEasy;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public class PresenceServiceTests
+    {
+        private readonly IMessageSender<XmlElement> messageSender;
+        private readonly Jid jid;
+        private readonly PresenceService presenceService;
+
+        public PresenceServiceTests()
+        {
+            messageSender = A.Fake<IMessageSender<XmlElement>>();
+            jid = $"{Guid.NewGuid()}@domain.com";
+            presenceService = new PresenceService(messageSender, jid);
+        }
+
+        [Fact]
+        public async Task UpdateStatusAsync_Sends_Correct_Presence_Stanza()
+        {
+            var expectedStatus = new PresenceStatus
+            {
+                AvailabilitySubstate = AvailabilitySubstate.Chat,
+                StatusText = Guid.NewGuid().ToString()
+            };
+            var token = new CancellationToken();
+            var sentStanza = default(XmlElement);
+            A.CallTo(() => messageSender.SendAsync(A<XmlElement>._, token)).Invokes(foc => sentStanza = foc.GetArgument<XmlElement>(0));
+
+            await presenceService.UpdateStatusAsync(expectedStatus, token);
+
+            Assert.NotNull(sentStanza);
+        }
+    }
+}
