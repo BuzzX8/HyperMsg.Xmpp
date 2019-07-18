f119e78cfae1154a37bfd44c926926ff22a96024
Maxim Chasnik <maksym.c@sbtech.com>
2019-07-18 09:33:31 +0300

    wip

---
+++ src/HyperMsg.Xmpp.Client/PresenceSubscriptionService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public class PresenceSubscriptionService : IPresenceSubscriptionService
+    {
+        private readonly IMessageSender<XmlElement> messageSender;
+        private readonly Jid jid;
+
+        public PresenceSubscriptionService(IMessageSender<XmlElement> messageSender, Jid jid)
+        {
+            this.messageSender = messageSender ?? throw new ArgumentNullException(nameof(messageSender));
+            this.jid = jid ?? throw new ArgumentNullException(nameof(jid));
+        }
+
+        public Task ApproveSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CancelSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RejectSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RequestSubscriptionAsync(Jid subscriptionJid, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UnsubscribeAsync(Jid subscriptionJid, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<Jid> SubscriptionRequested;
+
+        public event Action<Jid> SubscriptionCanceled;
+
+        public event Action<Jid> SubscriptionRejected;
+
+        public event Action<Jid> Unsubscribed;
+    }
+}
---
+++ test/HyperMsg.Xmpp.Client.Tests/PresenceSubscriptionServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using FakeItEasy;
+using System;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public class PresenceSubscriptionServiceTests
+    {
+        private readonly IMessageSender<XmlElement> messageSender;
+        private readonly Jid jid;
+        private readonly PresenceSubscriptionService service;
+
+        public PresenceSubscriptionServiceTests()
+        {
+            messageSender = A.Fake<IMessageSender<XmlElement>>();
+            jid = $"{Guid.NewGuid()}@domain.com";
+            service = new PresenceSubscriptionService(messageSender, jid);
+        }
+    }
+}
