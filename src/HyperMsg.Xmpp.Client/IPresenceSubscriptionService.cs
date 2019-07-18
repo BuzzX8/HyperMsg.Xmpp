2eef991da9ca9ce3e4a10912133ffd3a5264fd7c
Maxim Chasnik <maksym.c@sbtech.com>
2019-07-18 08:34:29 +0300

    wip

---
+++ src/HyperMsg.Xmpp.Client/IPresenceSubscriptionService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public interface IPresenceSubscriptionService
+    {
+        Task RequestSubscriptionAsync(Jid subscriptionJid, CancellationToken cancellationToken);
+
+        Task UnsubscribeAsync(Jid subscriptionJid, CancellationToken cancellationToken);
+
+        Task ApproveSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken);
+
+        Task RejectSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken);
+
+        Task CancelSubscriptionAsync(Jid subscriberJid, CancellationToken cancellationToken);
+
+        event Action<Jid> SubscriptionRequested;
+
+        event Action<Jid> SubscriptionCanceled;
+
+        event Action<Jid> SubscriptionRejected;
+
+        event Action<Jid> Unsubscribed;
+    }
+}
