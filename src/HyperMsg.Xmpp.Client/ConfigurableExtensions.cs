90fdd97a345e69ed51250b8d8e90dae5e8b97944
BuzzX8 <maxim.chasnik@gmail.com>
2019-09-07 18:25:45 +0300

    wip

---
+++ src/HyperMsg.Xmpp.Client/ConfigurableExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace HyperMsg.Xmpp.Client
+{
+    public static class ConfigurableExtensions
+    {
+        public static void UseRosterService(this IConfigurable configurable)
+        {
+            configurable.RegisterService(typeof(IRosterService), (p, s) =>
+            {
+                var sender = (IMessageSender<XmlElement>)p.GetService(typeof(IMessageSender<XmlElement>));
+                var registry = (IMessageHandlerRegistry<XmlElement>)p.GetService(typeof(IMessageHandlerRegistry<XmlElement>));
+
+                var service = new RosterService(sender);
+                registry.Register(service.Handle);
+
+                return service;
+            });
+        }
+
+        public static void UsePresenceService(this IConfigurable configurable)
+        {
+            configurable.RegisterService(typeof(IPresenceService), (p, s) =>
+            {
+                var sender = (IMessageSender<XmlElement>)p.GetService(typeof(IMessageSender<XmlElement>));
+                var registry = (IMessageHandlerRegistry<XmlElement>)p.GetService(typeof(IMessageHandlerRegistry<XmlElement>));
+
+                var service = new PresenceService(sender);
+                registry.Register(service.Handle);
+
+                return service;
+            });
+        }
+
+        public static void UsePresenceSubscriptionService(this IConfigurable configurable)
+        {
+            configurable.RegisterService(typeof(IPresenceSubscriptionService), (p, s) =>
+            {
+                var sender = (IMessageSender<XmlElement>)p.GetService(typeof(IMessageSender<XmlElement>));
+                var registry = (IMessageHandlerRegistry<XmlElement>)p.GetService(typeof(IMessageHandlerRegistry<XmlElement>));
+
+                var service = new PresenceSubscriptionService(sender);
+                registry.Register(service.Handle);
+
+                return service;
+            });
+        }
+        
+        public static void UseMessageService(this IConfigurable configurable)
+        {
+            configurable.RegisterService(typeof(IMessageService), (p, s) =>
+            {
+                var sender = (IMessageSender<XmlElement>)p.GetService(typeof(IMessageSender<XmlElement>));
+                var registry = (IMessageHandlerRegistry<XmlElement>)p.GetService(typeof(IMessageHandlerRegistry<XmlElement>));
+
+                var service = new MessageService(sender);
+                registry.Register(service.Handle);
+
+                return service;
+            });
+        }
+    }
+}
