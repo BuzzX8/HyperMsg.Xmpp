9135d8cda52ac79e903466790a60b835a641b3de
Maxim Chasnik <maksym.c@sbtech.com>
2019-09-11 15:13:38 +0300

    wip

---
+++ test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
@@ -0,0 +1,25 @@
+﻿using HyperMsg.Xmpp.Client.Components;
+using Xunit;
+
+namespace HyperMsg.Xmpp.Client
+{
+    public class ConnectionTests
+    {
+        private readonly ConnectionComponent connectionComponent;
+        private readonly XmppConnectionSettings settings;
+        private readonly Jid userJid;
+
+        public ConnectionTests()
+        {
+            userJid = "user@domain.com";
+            settings = new XmppConnectionSettings(userJid);
+            connectionComponent = new ConnectionComponent(null, settings);
+        }
+
+        [Fact]
+        public void Transport_Open()
+        {
+
+        }
+    }
+}
