b391923d6d76f8a72ac65c89eeba38026fb047af
Maxim Chasnik <maksym.c@sbtech.com>
2019-09-17 09:27:27 +0300

    wip

--- test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
+++ test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
@@ -10,12 +10,12 @@ using Xunit;
 namespace HyperMsg.Xmpp.Client
 {
     public class ConnectionTests : IntegrationFixtureBase<XmlElement>
-    {
+    {        
         private readonly ConnectionComponent connectionComponent;
         private readonly XmppConnectionSettings settings;
         private readonly Jid userJid;
-
-        private readonly CancellationTokenSource tokenSource;
+                
+        private readonly CancellationTokenSource tokenSource;        
 
         public ConnectionTests()
         {
@@ -36,15 +36,12 @@ namespace HyperMsg.Xmpp.Client
             await Task.Delay(1000);
         }
 
-        protected override void ConfigureSerializer(IConfigurable configurable)
-        {
-            configurable.RegisterService(typeof(ISerializer<XmlElement>), (p, s) => new XmppSerializer());
-        }
+        protected override void ConfigureSerializer(IConfigurable configurable) => configurable.RegisterService(typeof(ISerializer<XmlElement>), (p, s) => new XmppSerializer());
 
         protected override void ConfigureTransport(IConfigurable configurable)
         {
-            var adddresses = Dns.GetHostAddresses("jabber.org");
-            configurable.UseSockets(new IPEndPoint(adddresses[0], XmppConnectionSettings.DefaultPort));
+            var endPoint = new IPEndPoint(IPAddress.Loopback, XmppConnectionSettings.DefaultPort);
+            configurable.UseSockets(endPoint);
         }
     }
 }
