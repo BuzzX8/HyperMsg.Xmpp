0d212ae5ddc936c759c111dc012e33f64596423e
Maxim Chasnik <maksym.c@sbtech.com>
2019-09-17 08:03:01 +0300

    wip

--- test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
+++ test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
@@ -1,10 +1,17 @@
-﻿using HyperMsg.Xmpp.Client.Components;
+﻿using HyperMsg.Integration;
+using HyperMsg.Transport.Socket;
+using HyperMsg.Xmpp.Client.Components;
+using HyperMsg.Xmpp.Serialization;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace HyperMsg.Xmpp.Client
 {
-    public class ConnectionTests
+    public class ConnectionTests : IntegrationFixtureBase<XmlElement>
     {
+        
         private readonly ConnectionComponent connectionComponent;
         private readonly XmppConnectionSettings settings;
         private readonly Jid userJid;
@@ -13,13 +20,26 @@ namespace HyperMsg.Xmpp.Client
         {
             userJid = "user@domain.com";
             settings = new XmppConnectionSettings(userJid);
-            connectionComponent = new ConnectionComponent(null, settings);
+            connectionComponent = new ConnectionComponent(MessageSender, settings);
+            HandlerRegistry.Register(connectionComponent.HandleAsync);            
         }
 
         [Fact]
-        public void Transport_Open()
+        public async Task Transport_Open()
         {
+            
+            await Transport.ProcessCommandAsync(TransportCommand.Open, CancellationToken.None);
+        }
 
+        protected override void ConfigureSerializer(IConfigurable configurable)
+        {
+            configurable.RegisterService(typeof(ISerializer<XmlElement>), (p, s) => new XmppSerializer());
+        }
+
+        protected override void ConfigureTransport(IConfigurable configurable)
+        {
+            var adddresses = Dns.GetHostAddresses("jabber.org");
+            configurable.UseSockets(new IPEndPoint(adddresses[0], XmppConnectionSettings.DefaultPort));
         }
     }
 }
