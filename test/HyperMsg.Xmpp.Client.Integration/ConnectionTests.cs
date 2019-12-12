670d520d9a1a90d889ccd16c0bd162d29575c7de
Maxim Chasnik <maksym.c@sbtech.com>
2019-12-12 14:34:24 +0200

    wip

--- test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
+++ test/HyperMsg.Xmpp.Client.Integration/ConnectionTests.cs
@@ -3,26 +3,36 @@ using HyperMsg.Socket;
 using System.Net;
 using Xunit;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HyperMsg.Xmpp.Client
 {
     public class ConnectionTests : IntegrationFixtureBase
     {
         const int DefaultBufferSize = 2048;
-              
-
         private static readonly IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, XmppConnectionSettings.DefaultPort);
+
+        private readonly XmppConnectionSettings connectionSettings;
+        private readonly List<XmlElement> receivedElements;
 
         public ConnectionTests() : base(DefaultBufferSize, DefaultBufferSize)
         {
-            Configurable.UseXmppServices(null);            
+            connectionSettings = new XmppConnectionSettings("user@domain.org");
+            Configurable.UseXmppServices(connectionSettings);
             Configurable.UseSockets(endPoint);
+            receivedElements = new List<XmlElement>();
+            HandlerRegistry.Register<Received<XmlElement>>(r =>
+            {
+                receivedElements.Add(r);
+            });
         }
 
         [Fact]
         public async Task Open_Xmpp_Connection_()
         {
             await OpenTransportAsync();
+
+            Assert.NotEmpty(receivedElements);
         }
     }
 }
